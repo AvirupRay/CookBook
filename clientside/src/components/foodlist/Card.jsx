1bf89738a00757086dc9ab1332ee18e7d37e439c
Avirup Ray <avirupraytun@gmail.com>
2024-03-10 18:50:09 +0530

    foodlist updated

--- clientside/src/components/foodlist/Card.jsx
+++ clientside/src/components/foodlist/Card.jsx
@@ -2,11 +2,9 @@ import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
 function Card() {
-  const [selectedId, setSelectedId] = useState(null);
-
   return (
     <>
-      <div className="h-fit w-full grid grid-cols-6 gap-5">
+      <div className="h-full w-fit grid grid-cols-6 gap-5">
         <div className="bg-[#F4E896] w-[30vw] h-[35vh] rounded-3xl col-span-2 col-start-1"></div>
         <div className="bg-[#262622] w-[60vw] h-[35vh] rounded-3xl col-span-4 col-start-3"></div>
         <div className="bg-[#262622] w-[60vw] h-[35vh] rounded-3xl  col-span-4 col-start-1"></div>
--- clientside/src/components/foodlist/Foodlist.jsx
+++ clientside/src/components/foodlist/Foodlist.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, NavLink } from "react-router-dom";
-import { SlArrowDown, SlArrowUp } from "react-icons/sl";
 import Card from "./Card";
-import Items from "./Items";
 import axios from "axios";
 
 function Foodlist() {
@@ -29,10 +27,12 @@ function Foodlist() {
       if (i == 0) text += " " + foods[i];
       else text += ", " + foods[i];
     }
-    text += " what can I make with it?";
+
+    text += " give me 4 recipie list seperated by @";
     const data = { text: text };
     axios.post("http://localhost:3000/api/Rec", data).then((res) => {
-      console.log(res.data); // return data gets console logged
+      console.log(res.data);
+      // return data gets console logged
       setFoods([]);
     });
   }
@@ -40,33 +40,51 @@ function Foodlist() {
   return (
     <>
       <div className="flex items-center justify-center flex-col bg-[#2E2D27] rounded-3xl gap-4 overflow-hidden">
-        <div className="bg-[#2E2D27] w-full h-[10vh] rounded-3xl p-5 flex justify-center items-end gap-4">
+        <div className="bg-[#2E2D27] w-full h-[10vh]  rounded-3xl p-5 flex justify-evenly gap-2 items-center">
           {/* Head */}
-          <input
-            type="text"
-            value={item}
-            onChange={(e) => {
-              setItem(e.target.value);
-            }}
-            placeholder="insert ingredients"
-            className=" outline-none py-1 px-3  rounded-3xl text-black font-mono font-semibold h-[5vh] mt-10 w-[60vw] cursor-text"
-          />
+          <div className="flex justify-center items-center gap-4">
+            <input
+              type="text"
+              value={item}
+              onChange={(e) => {
+                setItem(e.target.value);
+              }}
+              placeholder="insert ingredients"
+              className=" outline-none py-1 px-3  rounded-3xl text-black font-mono font-semibold h-[5vh] w-[60vw] cursor-text"
+            />
 
-          <button
-            onClick={add}
-            className="bg-[#d9d9d938] text-white font-bold font-mono text-2xl px-2 rounded-3xl h-[5vh]"
-          >
-            Add
-          </button>
+            <button
+              onClick={add}
+              className="bg-[#d9d9d938] text-white font-bold font-mono text-2xl px-4 rounded-3xl h-[5vh]"
+            >
+              Add
+            </button>
 
-          <button
-            onClick={generate}
-            className="bg-[#d9d9d938] text-white font-bold font-mono text-2xl px-2 rounded-3xl h-[5vh]"
-          >
-            Generate
-          </button>
-
+            <button
+              onClick={generate}
+              className="bg-[#d9d9d938] text-white font-bold font-mono text-2xl px-2 rounded-3xl h-[5vh]"
+            >
+              Generate
+            </button>
+          </div>
           {/* body */}
+          <div className=" bg-[#F4E896] w-80 h-20 rounded-3xl p-2">
+            <ul className="flex flex-row">
+              {foods.map((item, index) => {
+                return (
+                  <li key={index} className="text-black font-bold font-mono">
+                    {item}
+                    <button
+                      onClick={() => setFoods(foods.filter((i) => i !== item))}
+                      className="bg-[#000] text-white  text-m rounded-3xl w-6 h-6 mx-2"
+                    >
+                      X
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
 
         {/* food list */}
